fe052b384d77bb3da10a2f21c40d9ac79df9fa4b
Brandon Nuñez <brandondevnunez@gmail.com>
2024-06-05 01:51:21 -0500

    finalizando footer

---
+++ js/app.js
@@ -0,0 +1,18 @@
+const enlace = document.querySelector(".up a");
+const html = document.querySelector("html");
+const modeDark = document.querySelector(".mode-dark");
+const title = document.querySelector(".title");
+const btn = document.querySelector(".btn")
+
+enlace.addEventListener("click", ()=>{
+    enlace.href = "#header"
+    html.style.scrollBehavior = "smooth"
+})
+
+modeDark.addEventListener("click", ()=>{
+    if(html.classList.contains("black")){
+        html.classList.remove("black");
+    }else{
+        html.classList.add("black");
+    }
+})
